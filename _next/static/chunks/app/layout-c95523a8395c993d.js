c7034611a46558f7bff1df22b3197223cf366d2d
ojingwaDaniel <ojingwadanny@gmail.com>
2025-01-10 17:27:56 +0100

    Updates

---
+++ _next/static/chunks/app/layout-c95523a8395c993d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5549:(e,s,t)=>{Promise.resolve().then(t.t.bind(t,1660,23)),Promise.resolve().then(t.t.bind(t,347,23))},347:()=>{},1660:e=>{e.exports={style:{fontFamily:"'Outfit', 'Outfit Fallback'",fontStyle:"normal"},className:"__className_9b26ca"}}},e=>{var s=s=>e(e.s=s);e.O(0,[426,441,517,358],()=>s(5549)),_N_E=e.O()}]);
