c1bc39d25a1aa5c7a3ba6d0ac08edcfe7ec8f3e9
ojingwaDaniel <ojingwadanny@gmail.com>
2025-01-10 11:41:43 +0100

    made some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enables static export
+  assetPrefix: "./", // Ensures assets load correctly
+  basePath: "/care-just4U", // Replace 'your-repo-name' with your GitHub repository name
+};
 
 export default nextConfig;
