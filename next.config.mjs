649633faa384a11fb13afdeaecdfced9e19401a1
ojingwaDaniel <ojingwadanny@gmail.com>
2025-01-10 11:45:01 +0100

    made some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enables static export
-  assetPrefix: "./", // Ensures assets load correctly
   basePath: "/care-just4U", // Replace 'your-repo-name' with your GitHub repository name
 };
 
